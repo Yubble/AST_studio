d6d5ff687a6e566ef44d155af88ea3ad9cd343f5
刘燕保 <liuyanbao@shuidihuzhu.com>
2019-11-04 20:11:47 +0800

    feat: 第一步

--- esprima/demo.js
+++
@@ -1,7 +0,0 @@
-exports.esprimaParse = "let answer = 'test unknow' // 这是未知模式下的代码"
-exports.esprimaParseFun = "function fn1 () {"
-  +"return 111"
-  +"}"
-// parseModule方法可以处理带es module的语句，带es module的语句也只有这个方法可以解析
-exports.esprimaParseModule = "export const answer = 'test module'"
-exports.esprimaParseScript = "const answer = 'test script'"
--- esprima/options_loc.js
+++
@@ -1,7 +0,0 @@
-const esprima = require('esprima')
-const { esprimaParse } = require('./demo')
-require('colors')
-
-// 也可以用loc来定位代码的行列
-console.log('esprimaParse loc is '.magenta, esprima.parse(esprimaParse, { loc: true }).loc)
-// { start: { line: 1, column: 0 }, end: { line: 1, column: 40 } }
